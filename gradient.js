53ba812fb2460105d8af5dbd2bd5a596289365d1
Anton Zotov <azpk360@yandex.ru>
2017-03-04 16:55:44 +0500

    Update gradient.js

--- gradient.js
+++ gradient.js
@@ -9,6 +9,7 @@ class color_1 {
 	value_name(this.name){
 		var low_name = this.name.toLowerCase();
 		var n = 0;
+		var sum = 0;
 		var alphabet = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т',
 		'у','ф','х','ц','ч','щ','ш','ь','ы','ъ','э','ю','я'];
 		for(var i = 0; i < low_name.length; i++){
@@ -18,6 +19,11 @@ class color_1 {
 			}
 			low_name[i] = n;
 		}
+		for(var i = 0; i < low_name.length; i++){
+			sum += low_name[i];
+		}
+		return sum;
+	}
 }
 /* Цвет градиента 2 задается датой рождения.
 Для того, чтобы рассчитать кармическое число по дате рождения, следует последовательно сложить все числа, которые в ней присутствуют.*/
@@ -26,19 +32,35 @@ class color_2 {
 		this.date_of_brith = date_of_brith;
 	}
 	value_date_of_brith(this.date_of_brith){
+		var temp_sum;
 		var d_br = this.date_of_brith;
 		d_br = d_br+'.';
 		var n = 0;
 		var m = 0;
 		var i = 0;
-		var sum;
+		var sum = 0;
 		while(i < 4){
 			while(d_br[n] != '.'){
 				n++;
 			}
-			sum += d_br.substring(m,n-1);
-        	m = n-1;
+			if(d_br.substring(m,n-1) == 10){
+				sum += d_br.substring(m,n-1);
+			} else {
+				temp_sum = d_br.substring(m,n-1);
+				for(var p = 0; p < temp_sum.length; p++){
+					sum += temp_sum[i];
+				}
+			}
+        		m = n-1;
 			i++;
 		}
+		return sum;
+	}
+}
+/* Конвертирует полученные числа в код RGB */
+class convert_color {
+	constructor(val1, val2){
+		this.val1 = val1;
+		this.val2 = val2;
 	}
 }
