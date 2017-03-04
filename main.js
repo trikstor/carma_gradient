85a1369847fac49991e871d7a5e08cf6cf5ea107
Anton Zotov <azpk360@yandex.ru>
2017-03-04 17:48:31 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+angular.module('zotovscript', [])
+.controller('MyController', ['$scope', function($scope) {
+   $scope.retran = function() {
+     if($scope.name.length > 3 && $scope.name.length > 3) {
+       let color_1 = new color_1($scope.name);
+       var st1 = color_1.value_name();
+       let color_2 = new color_2($scope.date);
+       var st2 = color_2.value_date_of_brith();
+     } else {
+       $scope.result = "Заполните все поля.";
+   }
+}]);
